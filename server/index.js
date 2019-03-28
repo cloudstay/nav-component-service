83db6c15623d0735c8dd0e2374d64d93b8dd28d0
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-03-28 14:16:01 -0700

    Add port  number

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const port = process.env.PORT;
+const port = process.env.PORT || 3003;
 const app = express();
 
 app.use(bodyParser.json());
