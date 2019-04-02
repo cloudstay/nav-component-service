7bd885442151eac01589a58e2d281a83e724ece7
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-04-02 14:22:29 -0700

    Add randomly generated id number

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,10 +9,15 @@ import SleepingArrangements from './SleepingArrangements.jsx';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      listings: []
+    }
   }
 
+
   getRequest() {
-    axios.get('localhost:3003/api/rooms')
+    var randomListing = Math.floor(Math.random() * 100) + 100;
+    axios.get(`localhost:3003/api/rooms/${randomListing}`)
     .then((response) => {
       console.log('send a get request to the server')
     })
