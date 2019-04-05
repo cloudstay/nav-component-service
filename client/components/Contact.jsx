c9ff84b4ffadadaae110d10a212ae7179121d51f
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-04-05 12:01:34 -0700

    Complete description and created Contact component

---
+++ client/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = function() {
+  return (
+    <div style={{marginTop: 24}}>
+      <span className="info-header">
+        <button type="button" className="button" aria-expanded="false" aria-busy="false">
+          <a className="button" href="#">Contact Host</a>
+        </button>
+      </span>
+    </div>
+  )
+}
+
+export default Contact;
