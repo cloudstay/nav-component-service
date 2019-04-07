c2b001c59d36da5698c9776ecc9c515d60e89cfc
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-04-07 16:45:00 -0700

    Not add icons for Amenities but completed styling

--- client/components/Amenities.jsx
+++ client/components/Amenities.jsx
@@ -9,7 +9,10 @@ const customStyles = {
     right                 : 'auto',
     bottom                : 'auto',
     marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+    transform             : 'translate(-50%, -50%)',
+    overflow              : 'scroll',
+    height                : '600px',
+    width                 : '600px'
   }
 };
 
@@ -46,125 +49,59 @@ class Amenities extends React.Component {
     for (let key in amenities) {
       if(key !== '_id'){
         if(amenities[key]) {
-          amenitiesArray.push(<div key={uuidv4()}>{key}</div>)
+          amenitiesArray.unshift(<div key={uuidv4()}>{key}</div>)
         } else {
           amenitiesArray.push(<div key={uuidv4()}><s>{key}</s></div>)
         }
       }
     }
     return (
-      <div>
-        <div id="amenities">
+        <div className="listing-amenities">
           <div style={{marginBottom: 0}}>
-            <section>
-              <div style={{marginBottom: 16}}>
-                <h2 className="title-bold">
-                  <div className="sub-header">Amenities</div>
-                </h2>
-              </div>
-              <div className="main-container">
-                <div className="amenities-container">
-                  <div style={{marginBottom: 16}}>
-                    <table className="amenity-container">
-                      <tbody>
-                        <tr>
-                          <td className="amenity-icon">
-                            󱀁
-                          </td>
-                          <td className="amenity-name">
-                            <div className="amenity">{amenitiesArray.slice(0, 1)}</div>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                  <div style={{marginBottom: 16}}>
-                    <table className="amenity-container">
-                      <tbody>
-                        <tr>
-                          <td className="amenity-icon">
-                            󱀁
-                          </td>
-                          <td className="amenity-name">
-                            <div className="amenity">{amenitiesArray.slice(1, 2)}</div>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-                <div className="amenities-container">
-                    <div style={{marginBottom: 16}}>
-                      <table className="amenity-container">
-                        <tbody>
-                          <tr>
-                            <td className="amenity-icon">
-                              󱀁
-                            </td>
-                            <td className="amenity-name">
-                              <div className="amenity">{amenitiesArray.slice(2, 3)}</div>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                    <div style={{marginBottom: 16}}>
-                      <table className="amenity-container">
-                        <tbody>
-                          <tr>
-                            <td className="amenity-icon">
-                              󱀁
-                            </td>
-                            <td className="amenity-name">
-                              <div className="amenity">{amenitiesArray.slice(3, 4)}</div>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                </div>
-              </div>
-              <div style={{marginTop: 8}}>
-                <div className="info-header">
-                  <button type="button" className="button" aria-expanded="false" aria-busy="false" onClick={this.handleOpenModal}>
-                    Show all amenities
-                  </button>
-                  <div id="amenitiesModal">
-                    <Modal
-                      isOpen={this.state.modal}
-                      onAfterOpen={this.afterOpenModal}
-                      onRequestClose={this.handleCloseModal}
-                      style={customStyles}
-                      contentLabel="Amenities Modal"
-                    >
-                      <section>
-                        <div className="modal-section">
-                          <div className="close-container">
-                            <button type="button" className="button" aria-busy="false" style={{padding: 0 , margin: 0}} onClick={this.handleCloseModal}>
-                              󱀁
-                            </button>
-                          </div>
-                          <header>
-                            <div className="amenities-header-container">
-                              <h1 className="amenities-header">Amenities</h1>
-                            </div>
-                          </header>
-                          <section>
-                            <div style={{marginTop: 8}}>
-                              <div style={{marginTop: 16}}>
-                                <div style={{marginBottom: 16}}>{amenitiesArray}</div>
-                              </div>
-                            </div>
-                          </section>
-                        </div>
-                      </section>
-                    </Modal>
+            <div style={{marginBottom: 16}}>
+              <h2 className="title-bold">
+                <div className="sub-header">Amenities</div>
+              </h2>
+            </div>
+            <div className="listing-amenities-container">
+              <div className="amenities-container">
+                <div style={{marginBottom: 16}}>
+                  <div className="listing-amenities_wrapper">
+                    {amenitiesArray && amenitiesArray.slice(0, 4).map((item, index) =>
+                      <div className="amenity-name" key={index}>
+                        <span className="amenity">{item}</span>
+                      </div>
+                    )}
                   </div>
                 </div>
               </div>
-            </section>
+            </div>
           </div>
-        </div>
+          <div id="amenitiesModal" className="info-header">
+            <button type="button"  className="button" aria-expanded="false" aria-busy="false" onClick={this.handleOpenModal}>
+              <div className="show-all">
+                Show all amenities
+              </div>
+            </button>
+            <Modal
+                isOpen={this.state.modal}
+                onAfterOpen={this.afterOpenModal}
+                onRequestClose={this.handleCloseModal}
+                style={customStyles}
+                contentLabel="Amenities Modal"
+              >
+                <div className="close-container">
+                  <button type='button' className="close-btn" onClick={this.handleCloseModal}>X</button>
+                </div>
+                <h1 className="amenities-header">Amenities</h1>
+                  {amenitiesArray.map((item, index) =>
+                    <div className="listing-amenities-detail" key={index}>{item}</div>
+                )}
+              </Modal>
+          </div>
+          <div style={{marginTop: 24, marginBottom: 24}}>
+            <div className="line-break"></div>
+          </div>
       </div>
     )
   }
