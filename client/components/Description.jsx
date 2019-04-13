fb2778ea94a5fdb9d3392952e0ad0e55651d25b1
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-04-13 09:05:16 -0700

    Refactor the code

--- client/components/Description.jsx
+++ client/components/Description.jsx
@@ -20,43 +20,10 @@ class Description extends React.Component{
 
   render() {
     if(this.props.listing) {
-      if (!this.state.showMore) {
-        return (
-          <div id="details">
-            <div className="detail details-text text" aria-hidden="true">
-              {this.props.listing.description.slice(0, 250)}
-            </div>
-            <div style={{marginTop: 16}}>
-              <div className="dropdown">
-                <div style={{marginTop: 8, marginBottom: 24}}>
-                  <div className="info-header">
-                    <button type="button" className="button" aria-expanded="false" aria-busy="false" onClick={this.handleShowMore}>
-                      <div className="read-more">
-                        Read more about the space
-                      </div>
-                      <div className="read-more">
-                        <div style={{marginLeft: 8}}>
-                          <div className="dropdown-icon">
-                            <FontAwesomeIcon icon={faChevronDown}/>
-                          </div>
-                        </div>
-                      </div>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <Contact />
-            <div style={{marginTop: 24, marginBottom: 24}}>
-              <div className="line-break"></div>
-            </div>
-          </div>
-        )
-      }
       return (
         <div id="details">
           <div className="detail details-text text" aria-hidden="true">
-            {this.props.listing.description}
+            {this.props.listing.description.slice(0, 250)}
           </div>
           <div style={{marginTop: 16}}>
             <div className="dropdown">
@@ -64,12 +31,12 @@ class Description extends React.Component{
                 <div className="info-header">
                   <button type="button" className="button" aria-expanded="false" aria-busy="false" onClick={this.handleShowMore}>
                     <div className="read-more">
-                      Hide
+                      {this.state.showMore ? "Hide" : "Read more about the space"}
                     </div>
                     <div className="read-more">
                       <div style={{marginLeft: 8}}>
                         <div className="dropdown-icon">
-                          <FontAwesomeIcon icon={faChevronUp}/>
+                          <FontAwesomeIcon icon={faChevronDown}/>
                         </div>
                       </div>
                     </div>
